1bb3978926598f0402388ac70eb2e98c253ad87c
Ali Akhtarzada <ali@aliak.net>
2010-05-28 17:14:51 +0100

    Done with initial static pages

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe PagesController do
+  integrate_views  
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag "title", "QF - Home"
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag "title", "QF - Contact"
+    end
+  end
+  
+  describe "Get 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag "title", "QF - About"
+    end
+  end
+end
