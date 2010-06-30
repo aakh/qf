fac8ad1a041b6bcca1ff86f07053ee3af32368e8
Ali Akhtarzada <ali@aliak.net>
2010-07-01 03:29:39 +1200

    All the ratings are being shown correctly now. Boolean and ordinal values are being taken into account. DONE! more or less...
    Removed the hide/display stuff for the value dimensions and ratings.
    Should probably take into account the actual values of the facts. It's all subjective now.
    Make it easier to rate and understand about belief systems.... somehow.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  DISTANCE_ALGO = :cristian
+  
   def title
     base_title = "QF"
     if @title.nil?
@@ -37,7 +40,70 @@ module ApplicationHelper
   end
   
   def show_entity_distance(entity)
-    display_bar 5, 10
+    case DISTANCE_ALGO
+    when :cristian then show_entity_distance_cristian(entity)
+    end
+  end
+  
+  def show_entity_distance_cristian(entity)
+    dist = 0
+    total = 0
+    i = 0
+    entity.concept.opinion_dimensions.each do |dim|
+      #For each dimension that this entity can be rated over
+      # Get ideal and weight value
+      # Get rating for this entity over the dimension
+      # Get percentage of rating within ideal
+      opinion = dim.valuable
+      
+      ideal = opinion.total_ideal / opinion.num_ideals
+      weight = opinion.total_weight / opinion.num_weights
+
+      # If no ideal then there's nothing to calculate the distance from, carry on
+      next unless opinion.num_ideals > 0
+      
+      # if weight is I don't care then this dimension should be ignored
+      next if weight == 1
+      
+      weight = 4 unless opinion.num_weights > 0
+      weight = (weight - 1) / 4
+      
+      # Boolean ideals are between 0 and 1, but the actual fact values
+      # Are either 0 or 1.
+      if opinion.dimension.bool?
+        fv = FactValue.get_value(Fact.find_by_name(dim.name), entity)
+        next unless fv
+        rating = if fv then fv.value else 1 end
+        rating = rating * 4 + 1
+        ideal = ideal * 4 + 1
+      else
+        cr = CurrentRating.find_by_entity_id_and_opinion_id(entity, opinion)
+        # If this dimension doesn't have a rating then carry on with the next.
+        next unless cr
+        rating = if cr.num_ratings > 0 then cr.total_rating / cr.num_ratings else 1 end
+      end
+
+      # Simple distance calculation
+      # dist += ((ideal - rating).abs + 1) * weight
+      
+      x = (1 - rating) / (1 - 5)
+      y = (1 - ideal) / (1 - 5)
+      i += 1
+      
+      next if x == y
+      
+      x *= weight
+      y *= weight
+      dist += 0.5 * (1 + (x - y).abs - (1 - x - y).abs)
+
+    end
+    
+    if i > 0
+      #return dist.to_s
+      display_bar(i - dist, i)
+    else
+      "Not available"
+    end
   end
   
   def show_rater(dim, key, value)
